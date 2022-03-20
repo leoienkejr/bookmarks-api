6078def88c98dff5d3767f4642ab3f4c98edace2
leoienkejr <leonardoienkejr@gmail.com>
2022-03-20 02:11:34 -0300

    Add app metadata

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from .routers.api import api
 
-app = FastAPI()
+app = FastAPI(title='Bookmarks API',
+              description='A RESTful bookmarks API.',
+              version='0.1.0')
+
 app.include_router(api)
