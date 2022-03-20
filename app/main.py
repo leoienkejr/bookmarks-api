ea629bc81fd9a5d0bf1566ede7e2550e181de0b3
leoienkejr <leonardoienkejr@gmail.com>
2022-03-20 02:54:37 -0300

    Make app name mutable from .env

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from .routers.api import api
+from .config import get_settings
 
-app = FastAPI(title='Bookmarks API',
+settings = get_settings()
+
+app = FastAPI(title=settings.app_name,
               description='A RESTful bookmarks API.',
               version='0.1.0')
 
