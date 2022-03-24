97ee4004cb8d83c5b2ba7363b4857794777075ca
leoienkejr <leonardoienkejr@gmail.com>
2022-03-24 01:52:29 -0300

    BA-4 Implement database connection and initialization

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
-from .routers.api import api
-from .config import get_settings
 
-settings = get_settings()
+from . import models
+from .config import Settings, get_settings
+from .db.db import engine
+from .routers.api import api
+
+
+settings: Settings = get_settings()
 
 app = FastAPI(title=settings.app_name,
               description='A RESTful bookmarks API.',
               version='0.1.0')
 
 app.include_router(api)
+
+
+@app.on_event('startup')
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(models.Base.metadata.create_all)
---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from ..db.db import Base
+
+_base = Base()
