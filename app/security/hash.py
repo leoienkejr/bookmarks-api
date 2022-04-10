6db9808866f3f8561839333dfb5886cae6b31dd8
leoienkejr <leonardoienkejr@gmail.com>
2022-04-10 18:38:41 -0300

    BA-7 Create security modules and functions for password hashing and verification

---
+++ app/security/hash.py
@@ -0,0 +1,17 @@
+'''
+Functions for hashing and verification of passwords
+'''
+
+
+def hash_password(password: str) -> str:
+    '''
+    Apply hashing to a password
+    '''
+    return password
+
+
+def verify_password(password: str, hash: str) -> bool:
+    '''
+    Verify if a password matches a hash
+    '''
+    return True
