a15f199e3c34204e12faa99b553a9e373f5ce86d
leoienkejr <leonardoienkejr@gmail.com>
2022-05-05 23:30:27 -0300

    In create_new_user(), implement handling of cases where an user with the given email already exists

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,15 +2,23 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from ..dependencies.get_session import get_session
 from ..security.hash import hash_password
-from ..crud.user import create_user
+from ..crud.user import create_user, get_user_by_email
 from ..schemas.user import UserCreate, UserRead
+from ..schemas.application_error import ApplicationErrorResponse
+from ..exceptions.exceptions import EmailAlreadyInUseError
 
 v1_user = APIRouter(prefix='/users')
 
 
-@v1_user.post('/', tags=['users'], response_model=UserRead)
+@v1_user.post('/', tags=['users'], response_model=UserRead, status_code=201,
+              responses={400: {'model': ApplicationErrorResponse}})
 async def create_new_user(user: UserCreate, db: AsyncSession = Depends(get_session)):
     async with db as db:
         async with db.begin():
+            existing_user = await get_user_by_email(db=db, email=user.email)
+
+            if existing_user is not None:
+                raise EmailAlreadyInUseError
+
             user = await create_user(db=db, email=user.email, password=user.password, hash_func=hash_password)
             return user
